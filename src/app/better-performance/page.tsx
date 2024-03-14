004ff4f050709614e8d3ed16bc018433285cda2e
Harshtyagi001 <96621226+Harshtyagi001@users.noreply.github.com>
2024-03-14 15:32:35 +0530

    Added loader UI

--- src/app/better-performance/page.tsx
+++ src/app/better-performance/page.tsx
@@ -1,21 +1,35 @@
+import HeatMap from "../component/heatmap";
 import getSkillAccuracy from "../lib/fetchSkillsPipeline"
+import { Course } from '../lib/models';
 
 export default async function BetterPerformance(){
 
   const skillsDetails=await getSkillAccuracy();
   console.log("skillsDetails on page: ",skillsDetails)
+
+  if (!skillsDetails) {
+    // Handle the case where skillsDetails is undefined
+    return <div>No skill details available</div>;
+  }
+      // Convert skill accuracy data to Course objects
+      const skills: Course[] = Object.entries(skillsDetails).map(([skill, accuracy]) => ({
+        name: skill,
+        marks: accuracy,
+      }));
+
   return <>
-   <h1>Better Performance</h1>
+   <h1>Better Performance Page</h1>
    {skillsDetails && (
         <div>
-          <h2>Skill Accuracy</h2>
-          <ul>
+          {/* <h2>Skill Accuracy</h2> */}
+          {/* <ul>
             {Object.entries(skillsDetails).map(([skill, accuracy]) => (
               <li key={skill}>
                 {skill}: {accuracy}%
               </li>
             ))}
-          </ul>
+          </ul> */}
+          <HeatMap courses={skills}/>
         </div>
       )}
   </>
--- src/app/lib/fetchSkillsPipeline.ts
+++ src/app/lib/fetchSkillsPipeline.ts
@@ -26,7 +26,7 @@ export default async function getSkillAccuracy() {
     // Define the aggregation pipeline
     const pipeline = [
       { $sort: { createdAt: -1 } }, // Sort by timestamp in descending order
-      { $limit: 5 }, // Limit to last 5 mock tests
+      { $limit: 2 }, // Limit to last 5 mock tests
       {
         $lookup: {
           from: 'questions',
@@ -83,13 +83,12 @@ export default async function getSkillAccuracy() {
     ];
 
     // Execute the aggregation pipeline
-    const result = await database.collection('practice-mocks').aggregate(pipeline).toArray();
+    const result = await database.collection('my-mocks').aggregate(pipeline).toArray();
 
     // Extract skill accuracy map from the result
     const skillAccuracyMap:SkillData[] = result.length > 0 ? result[0].skillAccuracyMap : [];
 
     console.log('Skill Accuracy Map:', skillAccuracyMap);
-    
 
   const skillAccuracy: SkillAccuracy = {};
 
